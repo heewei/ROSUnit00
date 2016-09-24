44d03f3fd04b779befbf80ecdd3c2ac2b8e185fe
HW Lim <heewei84@gmail.com>
2016-09-24 04:55:05 -0400

    1st Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/hewei/rosWS/src
+# Build directory: /home/hewei/rosWS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(tutorial)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/hewei/rosWS/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/hewei/rosWS/devel;/opt/ros/indigo".split(';') if "/home/hewei/rosWS/devel;/opt/ros/indigo" != "" else []
