e9f999e4ac3ec37664c143e0dcb8976ee51db268
HW Lim <heewei84@gmail.com>
2016-09-24 08:03:12 -0400

    Testing removing include

--- build/tutorial/CMakeFiles/_catkin_empty_exported_target.dir/DependInfo.cmake
+++ build/tutorial/CMakeFiles/_catkin_empty_exported_target.dir/DependInfo.cmake
@@ -16,7 +16,7 @@ SET(CMAKE_TARGET_LINKED_INFO_FILES
 
 # The include file search paths:
 SET(CMAKE_C_TARGET_INCLUDE_PATH
-  "/home/hewei/rosWS/src/tutorial/inc"
+  "/home/hewei/rosWS/src/tutorial/include"
   "/opt/ros/indigo/include"
   )
 SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
