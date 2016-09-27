183c2ab1926e6b4eceab7fa30588fafd9575307f
HW Lim <heewei84@gmail.com>
2016-09-27 05:21:37 -0400

    Test remove $CATKIN in catkin_package

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/hewei/rosWS/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/hewei/rosWS/devel/lib:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib:/opt/ros/jade/lib"
+export PKG_CONFIG_PATH="/home/hewei/rosWS/devel/lib/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig:/opt/ros/jade/lib/pkgconfig"
 export PWD="/home/hewei/rosWS/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hewei/rosWS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/hewei/rosWS/src:$ROS_PACKAGE_PATH"
